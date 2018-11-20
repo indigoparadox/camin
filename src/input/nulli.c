963d00caab45d0f7266bf876beb8eebc2cdad451
indigoparadox <indigo.repo@interfinitydynamics.info>
2018-11-20 12:56:21 -0500

    Initial revision based on files from isekai.

---
+++ src/input/nulli.c
@@ -0,0 +1,17 @@
+
+#define INPUT_C
+#include "../input.h"
+
+void input_init( struct INPUT* p ) {
+
+}
+
+void input_get_event( struct INPUT* input ) {
+   return;
+}
+
+void input_clear_buffer( struct INPUT* input ) {
+}
+
+void input_shutdown( struct INPUT* input ) {
+}
