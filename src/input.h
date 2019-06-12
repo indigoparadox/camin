306d002f6b6aa09542969f6773b2f298f37d6ee9
indigoparadox <indigo.repo@interfinitydynamics.info>
2019-06-12 16:41:36 -0400

    Should be engine-agnostic, now.

--- src/input.h
+++ src/input.h
@@ -3,49 +3,8 @@
 
 #include <stdint.h>
 
-typedef enum _INPUT_TYPE {
-   INPUT_TYPE_NONE,
-   INPUT_TYPE_KEY,
-   INPUT_TYPE_MOUSE,
-   INPUT_TYPE_CLOSE
-} INPUT_TYPE;
-
-struct INPUT {
-   void* event;
-   uint16_t character;
-   uint16_t scancode;
-   INPUT_TYPE type;
-   uint16_t repeat;
-};
-
-//#include "scaffold.h"
-//#include "client.h"
-
-#ifdef USE_ALLEGRO
-#include "allegro/keyboard.h"
-#endif /* USE_ALLEGRO */
-
-typedef enum INPUT_SCANCODE {
+enum INPUT_SCANCODE {
    INPUT_SCANCODE_NULL = 0,
-#ifdef USE_ALLEGRO
-   INPUT_SCANCODE_ESC = __allegro_KEY_ESC,
-   INPUT_SCANCODE_BACKSPACE = __allegro_KEY_BACKSPACE,
-   INPUT_SCANCODE_ENTER = __allegro_KEY_ENTER,
-   INPUT_SCANCODE_TAB = __allegro_KEY_TAB,
-   INPUT_SCANCODE_LEFT = __allegro_KEY_LEFT,
-   INPUT_SCANCODE_RIGHT = __allegro_KEY_RIGHT,
-   INPUT_SCANCODE_UP = __allegro_KEY_UP,
-   INPUT_SCANCODE_DOWN = __allegro_KEY_DOWN
-#elif defined( USE_SDL )
-   INPUT_SCANCODE_BACKSPACE = 22,
-   INPUT_SCANCODE_ENTER = 36,
-   INPUT_SCANCODE_ESC = 9,
-   INPUT_SCANCODE_TAB = 23,
-   INPUT_SCANCODE_LEFT = 82,
-   INPUT_SCANCODE_RIGHT = 83,
-   INPUT_SCANCODE_UP = 84,
-   INPUT_SCANCODE_DOWN = 85
-#else
    INPUT_SCANCODE_BACKSPACE = 1,
    INPUT_SCANCODE_ENTER = 2,
    INPUT_SCANCODE_ESC = 3,
@@ -54,8 +13,7 @@ typedef enum INPUT_SCANCODE {
    INPUT_SCANCODE_RIGHT = 6,
    INPUT_SCANCODE_UP = 7,
    INPUT_SCANCODE_DOWN = 8
-#endif
-} INPUT_SCANCODE;
+};
 
 typedef enum INPUT_ASSIGNMENT {
    INPUT_ASSIGNMENT_ATTACK = ' ',
@@ -67,9 +25,29 @@ typedef enum INPUT_ASSIGNMENT {
    INPUT_ASSIGNMENT_UP = 'w'
 } INPUT_ASSIGNMENT;
 
+typedef enum _INPUT_TYPE {
+   INPUT_TYPE_NONE,
+   INPUT_TYPE_KEY,
+   INPUT_TYPE_MOUSE,
+   INPUT_TYPE_CLOSE
+} INPUT_TYPE;
+
+struct INPUT {
+   void* event;
+   uint16_t character;
+   enum INPUT_SCANCODE scancode;
+   INPUT_TYPE type;
+   uint16_t repeat;
+};
+
+//#include "scaffold.h"
+//#include "client.h"
+
 void input_init( struct INPUT* p );
 void input_get_event( struct INPUT* input );
 void input_clear_buffer( struct INPUT* input );
 void input_shutdown( struct INPUT* input );
+int input_scancode_to_native( enum INPUT_SCANCODE code );
+enum INPUT_SCANCODE input_native_to_scancode( int code );
 
 #endif /* INPUT_H */
--- src/input/sdli.c
+++ src/input/sdli.c
@@ -33,7 +33,7 @@ void input_get_event( struct INPUT* input ) {
 
       input->type = INPUT_TYPE_KEY;
       input->character = event->key.keysym.unicode & 0x00ff;
-      input->scancode = event->key.keysym.scancode;
+      input->scancode = input_native_to_scancode( event->key.keysym.scancode );
 #ifdef DEBUG_KEYS
       lg_debug( __FILE__, "Scancode: %d\n", input->scancode );
       lg_debug( __FILE__, "Char: %d\n", input->character );
@@ -52,4 +52,67 @@ void input_clear_buffer( struct INPUT* input ) {
 
 void input_shutdown( struct INPUT* input ) {
    mem_free( input->event );
+}
+
+int input_scancode_to_native( enum INPUT_SCANCODE code ) {
+   switch( code ) {
+   case INPUT_SCANCODE_BACKSPACE:
+      return 22;
+
+   case INPUT_SCANCODE_ENTER:
+      return 36;
+
+   case INPUT_SCANCODE_ESC:
+      return 9;
+
+   case INPUT_SCANCODE_TAB:
+      return 23;
+
+   case INPUT_SCANCODE_LEFT:
+      return 82;
+
+   case INPUT_SCANCODE_RIGHT:
+      return 83;
+
+   case INPUT_SCANCODE_UP:
+      return 84;
+
+   case INPUT_SCANCODE_DOWN:
+      return 85;
+
+   case INPUT_SCANCODE_NULL:
+   default:
+      return 0;
+   }
+}
+
+enum INPUT_SCANCODE input_native_to_scancode( int code ) {
+   switch( code ) {
+   case 22:
+      return INPUT_SCANCODE_BACKSPACE;
+
+   case 36:
+      return INPUT_SCANCODE_ENTER;
+
+   case 9:
+      return INPUT_SCANCODE_ESC;
+
+   case 23:
+      return INPUT_SCANCODE_TAB;
+
+   case 82:
+      return INPUT_SCANCODE_LEFT;
+
+   case 83:
+      return INPUT_SCANCODE_RIGHT;
+
+   case 84:
+      return INPUT_SCANCODE_UP;
+
+   case 85:
+      return INPUT_SCANCODE_DOWN;
+
+   default:
+      return INPUT_SCANCODE_NULL;
+   }
 }
